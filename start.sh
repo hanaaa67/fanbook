916b1f87f5a031a656b22b4a7a020a8d35dc2891
hanaaa67 <aghnafaqih@gmail.com>
2024-06-27 12:39:09 +0700

    Revisi kode program 1

--- start.sh
+++ start.sh
@@ -5,14 +5,14 @@ export PYTHONUNBUFFERED=true
 VIRTUALENV=.data/venv
 
 if [! -d $VIRTUALENV ]; then
-python3 -m venv $VIRTUALENV
+    python3 -m venv $VIRTUALENV
 fi
 
-if [! -f$VIRTUALENV/bin/pip ]; then
-curl --silent --show=error --retry 5 https://bootstrap.pypa.io/get-pip.py
+if [! -f $VIRTUALENV/bin/pip ]; then
+    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py
 fi
 
 $VIRTUALENV/bin/pip install -r requirements.txt
 
 $VIRTUALENV/bin/python3 app.py
-footer
+Footer
